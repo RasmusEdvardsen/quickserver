79980e620120d18ee32815212461ae551a5f3bd1
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2018-02-20 23:32:16 +0100

    getuserrooms returns list of room ids

--- server.js
+++ server.js
@@ -10,46 +10,53 @@ app.use(bodyParser.json()) // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
 db.connect()
+
 routes(app) //register the route
 app.listen(27017, function(){
   console.log('app listening on *:27017')
-})
-
-//getuserrooms('5a84b9be36fa0822e0efef3f')
-
-io.engine.generateId = (req) => {
-  return req._query.uid
-}
-
-io.on('connection', function(socket){
-  socket.on('getuserrooms', getuserrooms(userid))
-
-  //private events
-  socket.on('privatemessage', privatemessage(room, username, msg))
-  socket.on('privatecreate', privatecreate(name, email))
-  socket.on('privateadd', function(name, email){
-    //TODO: WAAAY LATER
-  })
 })
 
 http.listen(3000, function(){
   console.log('socket listening on *:3000')
 })
 
-//meta events
-function getuserrooms(uid){
+io.engine.generateId = (req) => {
+  return req._query.uid
+}
+
+io.on('connection', function(socket){
+  //meta events
+  socket.on('getuserrooms', function(uid){
+    getuserrooms(socket, uid)
+  })
+  //private events
+  socket.on('privatemessage',function(room, username, msg){
+    privatemessage(socket, room, username, msg)
+  })
+  socket.on('privatecreate', function(name, email){
+    privatecreate(socket, name, email)
+  })
+  /*socket.on('privateadd', function(uid, rid){
+    //TODO: WAAAY LATER
+  })*/
+})
+
+// Meta Events
+function getuserrooms(socket, uid){
   db.findRooms(uid, function(rtrnVal){
-    //TODO: return user rooms to user here
+    var roomIDs = [];
+    rtrnVal.forEach(function(element){
+      roomIDs.push(element._id)
+    })
+    io.to(socket.id).emit('userrooms', roomIDs)
   })
 }
 
 //private events
-function privatecreate(name, email){
+function privatecreate(socket, name, email){
+  //TODO: IF THEY ALREADY HAVE A ROOM, THEN GO TO SAME
   mongoosemodels.User.find({ email: email }, function(err, user){
-    if(err){
-      //TODO: Figure out what to do here
-      console.log(err)
-    }
+    if(err) throw err //TODO: Figure out what to do here
     db.save(new mongoosemodels.Room({
       date: new Date(),
       name: name,
@@ -60,16 +67,15 @@ function privatecreate(name, email){
       //If undefined, means user isn't connected.
       if(io.sockets.connected[user[0]._id])
         io.sockets.connected[user[0]._id].join(doc._id)
-      console.log(io.sockets.adapter.rooms)
 
       //TODO: make this
-      io.to(doc._id).emit("newroom", doc._id.toString())
+      io.to(doc._id).emit('newroom', doc._id.toString())
     })
 
     //TODO: NOTIFICATION ABOUT INVITE TO ROOM!
   })
 }
-function privatemessage(room, username, msg){
+function privatemessage(socket, room, username, msg){
   console.log(io.sockets.adapter.rooms)
   io.to(room).emit('newmessage', username + ": " + msg)
   db.save(new mongoosemodels.Message({
